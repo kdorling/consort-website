0a0d0b9bf596c2983f09ef5c223366696b33e75e
kdorling <kudorling@gmail.com>
2023-05-16 12:02:15 -0600

    Add production settings

--- consort/consort/settings/production.py
+++ consort/consort/settings/production.py
@@ -1,6 +1,19 @@
 from .base import *
 
 DEBUG = False
+SECRET_KEY = "c^c7ycg_9)vzu+$k4w*=e9pa2vcl0cpu-a2!sfk5p5v5@5nxh#"
+ALLOWED_HOSTS = ["localhost", "138.197.131.37"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psychopg2",
+        "NAME": "consort_db",
+        "USER": "admin",
+        "PASSWORD": "conSort2023sql",
+        "HOST": "localhost",
+        "PORT": "",
+    }
+}
 
 try:
     from .local import *
