7ae10d09ac0535fb1909246fb240609fe2e44e23
kdorling <kudorling@gmail.com>
2023-07-21 15:07:03 -0600

    Add docker support

--- website/consort/settings/dev.py
+++ website/consort/settings/dev.py
@@ -1,13 +1,16 @@
 from .base import *
 
+import os
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=1))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-csov7al8s0(4^!6)pfzh$kc8wd(#smvj&679u()mo22ia9_v4!"
+SECRET_KEY = os.environ.get("SECRET_KEY", default="django-insecure-csov7al8s0(4^!6)pfzh$kc8wd(#smvj&679u()mo22ia9_v4!")
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default="localhost 127.0.0.1 [::1]").split(" ")
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
