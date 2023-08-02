8d2c45e1fdb330416cb15f19caeed60e0997223e
kdorling <kudorling@gmail.com>
2023-08-02 01:16:57 -0600

    Return businesses in search results

--- app/search/views.py
+++ app/search/views.py
@@ -9,6 +9,9 @@ from wagtail.documents.models import Document
 from wagtail.search.models import Query
 from wagtail.search.query import Fuzzy
 
+from business_directory.models import Business
+from custom_search.backends import get_search_backend
+
 
 def get_search_quote(search_result):
     if "information" in search_result.page_type_display_name.lower():
@@ -21,13 +24,20 @@ def search(request):
 
     # Search
     if search_query:
-        page_results = Page.objects.live().search(Fuzzy(search_query))
-        document_results  = Document.objects.search(search_query)
-        search_results = list(chain(page_results, document_results))
 
-        if len(search_results) == 0:
+        s = get_search_backend()
+
+        page_results = Page.objects.live().search(Fuzzy(search_query))
+        business_results = s.search(Fuzzy(search_query), Business.objects.filter(live=True))
+        document_results  = Document.objects.search(search_query)
+
+        if len(page_results) == 0:
             page_results = Page.objects.live().autocomplete(search_query)
-            search_results = list(chain(page_results, document_results))
+
+        if len(business_results) == 0:
+            business_results = s.autocomplete(search_query, Business.objects.filter(live=True))
+    
+        search_results = list(chain(page_results, document_results, business_results))
 
         query = Query.get(search_query)
 
@@ -45,6 +55,9 @@ def search(request):
     except EmptyPage:
         search_results = paginator.page(paginator.num_pages)
 
+    # result_map = {str(result): result for result in search_results}
+    # search_results = list(result_map.values())
+
     for result in search_results:
         highlights = set()
         if result.highlights_ and len(search_query) >= 3:        
@@ -54,7 +67,7 @@ def search(request):
                             highlights.add(highlight)
             result.highlights_ = highlights
 
-    search_results = [result for result in search_results if result.highlights_]
+    # search_results = [result for result in search_results if result.highlights_ or "page" in str(type(result)).lower()]
         
     return TemplateResponse(
         request,
