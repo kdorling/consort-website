f74415b4f657c194ea878d880868a3cbba671f89
kdorling <kudorling@gmail.com>
2023-08-02 01:17:07 -0600

    Temp

--- app/consort/settings/production.py
+++ app/consort/settings/production.py
@@ -28,7 +28,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
-'''
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
